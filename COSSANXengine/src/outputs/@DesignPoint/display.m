cfeb65435db5b1909df8d5ec5571f8c4b94ac470
Matteo Broggi <broggi@irz.uni-hannover.de>
2019-11-27 15:37:21 +0100

    Display FORM probability of failure
    Add the display of the probability of failure approximation given by FORM to the display method of DesignPoint

--- COSSANXengine/src/outputs/@DesignPoint/display.m
+++ COSSANXengine/src/outputs/@DesignPoint/display.m
@@ -17,18 +17,21 @@ OpenCossan.cossanDisp('=========================================================
 %% 2.   Design Point
 OpenCossan.cossanDisp('* Coordinates of the Design Point ',1);
 %   Standard normal space
-OpenCossan.cossanDisp(['** standard normal space: ' sprintf('%10.3e ',Xo.VDesignPointStdNormal)],2);
+OpenCossan.cossanDisp(['  - Standard normal space: ' sprintf('%10.3e ',Xo.VDesignPointStdNormal)],2);
 %   Physical space
-OpenCossan.cossanDisp(['** in physical space    : ' sprintf('%10.3e ',Xo.VDesignPointPhysical)],1);
+OpenCossan.cossanDisp(['  - Physical space       : ' sprintf('%10.3e ',Xo.VDesignPointPhysical)],1);
 
 %% 3.   Direction of design point
 OpenCossan.cossanDisp('* Unit Vector pointing in the direction of the Design Point ',1);
 %   Standard normal space
-OpenCossan.cossanDisp(['** standard normal space: ' sprintf('%10.3e ',Xo.VDirectionDesignPointStdNormal)],2);
+OpenCossan.cossanDisp(['  - Standard normal space: ' sprintf('%10.3e ',Xo.VDirectionDesignPointStdNormal)],2);
 %   Physical space
-OpenCossan.cossanDisp(['** in physical space    : ' sprintf('%10.3e ',Xo.VDirectionDesignPointPhysical)],1);
+OpenCossan.cossanDisp(['  - Physical space       : ' sprintf('%10.3e ',Xo.VDirectionDesignPointPhysical)],1);
 
 %% 4.   Euclidean norm
-OpenCossan.cossanDisp(['* Euclidean norm of the Design Point ' sprintf('%10.3e ',Xo.ReliabilityIndex)],2);
+OpenCossan.cossanDisp(['* Euclidean norm of the Design Point - factor of safety:' sprintf('%10.3e ',Xo.ReliabilityIndex)],2);
+
+%% 5.   FORM approximation of the probability of failure
+OpenCossan.cossanDisp(['* First order approximation of the failure probability :' sprintf('%10.3e ',Xo.form)],2);
 
 end
