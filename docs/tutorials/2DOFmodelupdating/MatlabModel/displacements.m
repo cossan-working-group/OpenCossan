d8ef6f649bd3343d2bf69798588258a2578ccc8f
Ander Gray <ander.gray@liverpool.ac.uk>
2020-02-13 11:53:32 +0000

    function handle for model eval

--- docs/tutorials/2DOFmodelupdating/MatlabModel/displacements.m
+++ docs/tutorials/2DOFmodelupdating/MatlabModel/displacements.m
@@ -1,17 +1,11 @@
-%% Displacement script
-%
-% This script computes 'n=length(Tinput)' times the displacement (yp) of the 2DOF system    
-n=length(Tinput); %Obtain the number of simulations that will be run
-%For each simulation
-for i=1:n  %Loop to run 'length(Tinput)' times the deterministic analysis
-    %Get the input values for each simulation and put in some more readable variable
-    k1=Tinput(i).k1;k2=Tinput(i).k2; 
-    F=[Tinput(i).F1 ; Tinput(i).F2];
-    %Determine the displacements for the 2DOF system (a simple FEA analysis may
-    %come here)
-    yp=([1.0/k1 1.0/k1; 1.0/k1 (k1+k2)/(k1*k2)])*F;
-    %Pass the resulted displacements for each simulation output values to
-    %the Toutput property for OpenCossan
-    Toutput(i).y1=yp(1);
-    Toutput(i).y2=yp(2);
+function out = displacements(in)
+    
+    out = table();
+    y = zeros(height(in), 2);
+    for i = 1:height(in)
+        y(i, :) = ([1.0 ./ in.k1(i) 1.0 ./ in.k1(i); 1.0 ./in.k1(i) (in.k1(i) + in.k2(i)) ./ (in.k1(i) .* in.k2(i))]) * [in.F1(i); in.F2(i)];
+    end
+    out.y1 = y(:, 1);
+    out.y2 = y(:, 2);
+    
 end
