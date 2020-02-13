21ff8acb57e8bd5a233098435780e25145a525c4
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2020-02-13 15:33:40 +0000

    Refactor Simples algorithm

--- +opencossan/+optimization/@Simplex/Simplex.m
+++ +opencossan/+optimization/@Simplex/Simplex.m
@@ -1,66 +1,19 @@
 classdef Simplex < opencossan.optimization.Optimizer
-    %SIMPLEX The simplex class define the optimizator SIMPLEX to solve
-    % unconstrained nonlinear problems  using a gradients free method 
+    %SIMPLEX The simplex class define the optimizator SIMPLEX to solve unconstrained nonlinear problems  using a gradients free method 
     
-      
-    %%  Methods
+    properties (Hidden)
+        ExitReasons = containers.Map([1, 0, -1],[
+            "The function converged to a solution x.", ...
+            "Number of iterations exceeded options.MaxIter or number of function evaluations exceeded options.MaxFunEvals.", ...
+            "The algorithm was terminated by the output function."]);
+    end
+    
     methods
-        varargout    = apply(Xobj,varargin)  % This method perform the simulation
-                                             % adopting the Xobj
+        function obj = Simplex(varargin)
+            %SIMPLEX            
+            obj@opencossan.optimization.Optimizer(varargin{:});
+        end
         
-        function Xobj   = Simplex(varargin)
-            %SIMPLEX    Constructor function for class Simplex
-            %
-            %   SequentialQuadraticProgramming
-            %
-            %   Simplex method is intended for solving unconstrained nonlinear
-            %   problem  using a gradinet free method. 
-            %
-            % =========================================================================
-            % COSSAN-X - The next generation of the computational stochastic analysis
-            % University of Innsbruck, Copyright 1993-2011 IfM
-            % =========================================================================
-            
-            % Argument Check
-            OpenCossan.validateCossanInputs(varargin{:})
-            
-            % Set predefined values
-            Xobj.Sdescription   = 'Simplex object';
-            Xobj.Nmax           = 1e3;
-            
-            % Process input arguments
-            for k=1:2:length(varargin)
-                switch lower(varargin{k})
-                    case 'sdescription'
-                        Xobj.Sdescription=varargin{k+1};
-                    case  {'nmax','nmaxmodelevaluations'}
-                        Xobj.Nmax=varargin{k+1};
-                    case  'scalingfactor'
-                        Xobj.scalingFactor=varargin{k+1};
-                    case  'xjobmanager'
-                        Xobj.XjobManager=varargin{k+1};
-                    case  'nmaxiterations'
-                        Xobj.NmaxIterations=varargin{k+1};
-                    case {'nseedrandomnumbergenerator'}
-                        Nseed       = varargin{k+1};
-                        Xobj.RandomNumberGenerator = ...
-                            RandStream('mt19937ar','Seed',Nseed);
-                    case {'xrandomnumbergenerator'}
-                        if isa(varargin{k+1},'RandStream'),
-                            Xobj.XrandomNumberGenerator  = varargin{k+1};    
-                        else
-                            warning('openCOSSAN:optimization:CrossEntropy',...
-                              ['argument associated with (' varargin{k} ') is not a RandStream object']);
-                        end  
-                    case  'toleranceobjectivefunction'
-                        Xobj.toleranceObjectiveFunction=varargin{k+1};
-                    case  'tolerancedesignvariables'
-                        Xobj.toleranceDesignVariables=varargin{k+1};
-                    otherwise
-                        warning('openCOSSAN:Simplex',...
-                            ['PropertyName ' varargin{k} ' not valid ']);
-                end
-            end % input check
-        end % constructor
-    end % methods
-end % classdef
+        varargout = apply(obj, varargin)
+    end
+end
--- +opencossan/+optimization/@Simplex/apply.m
+++ +opencossan/+optimization/@Simplex/apply.m
@@ -1,183 +1,83 @@
-function [Xoptimum, varargout] = apply(Xobj,varargin)
-%   APPLY   This method applies the algorithm
-%           Simplex for solving unconstrained problems
-%
-%
-% See Also: https://cossan.co.uk/wiki/apply@Simplex
-%
-% Author: Edoardo Patelli
-% Website: http://www.cossan.co.uk
-
-%{
-    This file is part of OpenCossan <https://cossan.co.uk>.
-    Copyright (C) 2006-2018 COSSAN WORKING GROUP
-
-    OpenCossan is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License or,
-    (at your option) any later version.
+function optimum = apply(obj, varargin)
+    %   APPLY   This method applies the algorithm
+    %           Simplex for solving unconstrained problems
+    %
+    %
+    % See Also: https://cossan.co.uk/wiki/apply@Simplex
+    %
+    % Author: Edoardo Patelli Website: http://www.cossan.co.uk
     
-    OpenCossan is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+    %{
+    This file is part of OpenCossan <https://cossan.co.uk>. Copyright (C) 2006-2018 COSSAN WORKING
+    GROUP
+
+    OpenCossan is free software: you can redistribute it and/or modify it under the terms of the GNU
+    General Public License as published by the Free Software Foundation, either version 3 of the
+    License or, (at your option) any later version.
     
-    You should have received a copy of the GNU General Public License
-    along with OpenCossan. If not, see <http://www.gnu.org/licenses/>.
-%}
-
-%% Define global variable for the objective function and the constrains
-global XoptGlobal XsimOutGlobal
-
-OpenCossan.validateCossanInputs(varargin{:});
-
-%  Check whether or not required arguments have been passed
-for k=1:2:length(varargin)
-    switch lower(varargin{k})
-        case {'xoptimizationproblem'}   
-            %check that arguments is actually an OptimizationProblem object
-            if isa(varargin{k+1},'OptimizationProblem')    
-                Xop     = varargin{k+1};
-            else
-                error('OpenCossan:Simplex:apply',...
-                    'The variable %s must be an OptimizationProblem object, provided object of type %s',...
-                    inputname(k),class(varargin{k+1}));
-            end
-        case {'cxoptimizationproblem'}   %extract OptimizationProblem
-            if isa(varargin{k+1}{1},'OptimizationProblem')    %check that arguments is actually an OptimizationProblem object
-                Xop     = varargin{k+1}{1};
-            else
-                error('OpenCossan:Simplex:apply',...
-                     ['the variable  ' inputname(k) ' must be an OptimizationProblem object']);
-            end  
-        case {'xoptimum'}   %extract OptimizationProblem
-            if isa(varargin{k+1},'Optimum')    %check that arguments is actually an OptimizationProblem object
-                Xoptimum  = varargin{k+1};
-            else
-                error('OpenCossan:Simplex:apply',...
-                    'The variable %s must be an Optimum, provided object of type %s',...
-                    inputname(k),class(varargin{k+1}));
-            end
-        case 'vinitialsolution'
-            VinitialSolution=varargin{k+1};
-        otherwise
-            error('OpenCossan:Simplex:apply',['the field ' varargin{k} ...
-                ' is not valid']);
-    end
+    OpenCossan is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+    even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License along with OpenCossan. If not,
+    see <http://www.gnu.org/licenses/>.
+    %}
+    
+    import opencossan.optimization.OptimizationRecorder;
+    import opencossan.common.utilities.*;
+    
+    [required, varargin] = parseRequiredNameValuePairs(...
+        "optimizationproblem", varargin{:});
+    
+    optProb = required.optimizationproblem;
+    
+    optional = parseOptionalNameValuePairs(...
+        "initialsolution", {optProb.InitialSolution}, ...
+        varargin{:});
+    
+    x0 = optional.initialsolution;
+    
+    assert(size(x0, 1) == 1, ...
+        'OpenCossan:MiniMax:apply',...
+        'Only 1 initial setting point is allowed')
+    
+    % Check inputs and initialize variables
+    obj = initializeOptimizer(obj);
+    
+    options = optimset('fminsearch');
+    options.Display = 'iter';
+    options.MaxFunEvals = obj.MaxFunctionEvaluations;
+    options.MaxIter = obj.MaxIterations;
+    options.TolFun = obj.ObjectiveFunctionTolerance;
+    options.TolX = obj.DesignVariableTolerance;
+    
+    memoizedModel = opencossan.optimization.memoizeModel(optProb);
+    
+    hobjfun=@(x)evaluate(optProb.ObjectiveFunctions,'optimizationproblem',optProb,...
+        'referencepoints',x,'model',memoizedModel,...
+        'scaling',obj.ObjectiveFunctionScalingFactor);
+    
+    assert(isempty(optProb.Constraints),  ...
+        'OpenCossan:Simplex:apply', ...
+        'Simplex is an UNconstrained Nonlinear Optimization.')
+   
+    opencossan.optimization.OptimizationRecorder.clear();
+    
+    startTime = tic;
+    
+    [optimalSolution, ~, exitFlag]  = fminsearch(hobjfun, x0, options);
+    
+    totalTime = toc(startTime);
+    
+    optimum = opencossan.optimization.Optimum(...
+        'optimalsolution', optimalSolution, ...
+        'exitflag', exitFlag, ...
+        'totaltime', totalTime, ...
+        'optimizationproblem', optProb, ...
+        'optimizer', obj, ...
+        'constraints', OptimizationRecorder.getInstance().Constraints, ...
+        'objectivefunction', OptimizationRecorder.getInstance().ObjectiveFunction, ...
+        'modelevaluations', OptimizationRecorder.getInstance().ModelEvaluations);
+    
+    if ~isdeployed; obj.saveOptimumToDatabase(optimum); end
 end
-
-%% Check Optimization problem
-assert(logical(exist('Xop','var')), 'OpenCossan:Simplex:apply:NoOptimizationObject',...
-    'Optimization problem must be defined')
-
-% Check inputs and initialize variables
-Xobj = initializeOptimizer(Xobj);
-
-%% Check initial solution
-if exist('VinitialSolution','var')
-    Xop.VinitialSolution=VinitialSolution;
-end
-
-assert(size(Xop.VinitialSolution,1)==1, ...
-    'OpenCossan:Simplex:apply',...
-    'Only 1 initial setting point is allowed')
-
-%% initialize Optimum
-if ~exist('Xoptimum','var')
-    XoptGlobal=Optimum('XoptimizationProblem',Xop,'Xoptimizer',Xobj);
-else
-    %TODO: Check Optimum
-    XoptGlobal=Xoptimum;
-end
-
-%%  Perform optimization
-%   Set matlab options
-Toptions            = optimset('fminsearch');  %Default optimization options
-Toptions.Display    = 'iter-detailed';   %Turns on intermediate information about optimization procedure
-Toptions.MaxFunEvals   = Xobj.Nmax;              %Maximum number of function evaluations that is allowed
-Toptions.MaxIter       = Xobj.NmaxIterations;          %Maximum number of iterations that is allowed
-Toptions.TolFun        = Xobj.toleranceObjectiveFunction;           %Termination tolerance on the value of the objective function
-Toptions.TolX          = Xobj.toleranceDesignVariables;       %Termination tolerance on the design variable vector
-Toptions.OutputFcn = @Xobj.outputFunctionOptimiser;
-
-% initialize global variable
-XsimOutGlobal=[];
-
-if isempty(Xop.Xmodel)
-    % Create handle of the objective function
-    hobjfun=@(x)evaluate(Xop.XobjectiveFunction,'Xoptimizationproblem',Xop,...
-        'MreferencePoints',x,'Lgradient',false,...        
-        'scaling',Xobj.scalingFactor);
-else
-    % Create handle of the objective function
-    hobjfun=@(x)evaluate(Xop.XobjectiveFunction,'Xoptimizationproblem',Xop,...
-        'MreferencePoints',x,'Lgradient',false,...
-        'scaling',Xobj.scalingFactor,'Xmodel',Xop.Xmodel);
-end
-
-
-assert(logical(isempty(Xop.Xconstraint)), ...
-    'OpenCossan:Simplex:apply',...
-    'Simplex is an Unconstrained Nonlinear Optimization. It can not be used to solved constrained problems')
-
-
-% The function that computes the nonlinear inequality constraints c(x)≤ 0
-% and the nonlinear equality constraints ceq(x) = 0. hconstrains accepts a
-% vector x and returns the two vectors c and ceq. c is a vector that
-% contains the nonlinear inequalities evaluated at x, and ceq is a vector
-% that contains the nonlinear equalities evaluated at x.  hconstrains is
-% a function handle such as function
-
-OpenCossan.setLaptime('Sdescription',['SIMPLEX:' Xobj.Sdescription]);
-
-
-%% Perform Real optimization
-[XoptGlobal.VoptimalDesign,XoptGlobal.VoptimalScores,Nexitflag]  = ...
-    fminsearch(hobjfun,Xop.VinitialSolution,Toptions);
-
-OpenCossan.setLaptime('Sdescription','End Simplex optimization');
-
-%% Output
-% All the quantities of interest are automatically stored in the Optimum
-% object.
-
-% Prepare string with reason for termination of optimization algorithm
-switch Nexitflag
-    case{1}
-        Sexitflag   = 'Converged to a solution';
-    case{0}
-        Sexitflag   = 'Number of iterations exceeded options.MaxIter or number of function evaluations exceeded options.MaxFunEvals';
-    case{-1}
-        Sexitflag   = 'Algorithm was terminated by the output function';
-end
-
-XoptGlobal.Sexitflag=Sexitflag;
-
-% Assign outputs
-Xoptimum=XoptGlobal;
-
-% Export Simulation Output
-varargout{1}    = XsimOutGlobal; 
-
-if ~isdeployed
-    % add entries in simulation and analysis database at the end of the
-    % computation when not deployed. The deployed version does this with
-    % the finalize command
-    XdbDriver = OpenCossan.getDatabaseDriver;
-    if ~isempty(XdbDriver)
-        XdbDriver.insertRecord('StableType','Result',....
-            'Nid',getNextPrimaryID(OpenCossan.getDatabaseDriver,'Result'),...
-            'CcossanObjects',{Xoptimum},...
-            'CcossanObjectsNames',{'Xoptimum'});
-    end
-end
-%% Delete global variables
-clear global XoptGlobal XsimOutGlobal 
-
-%%  Set random number generator to state prior to running simulation
-if exist('XRandomNumberGenerator','var')
-    Simulations.restoreRandomNumberGenerator(XRandomNumberGenerator)
-end
-
-%% Record Time
-OpenCossan.setLaptime('Sdescription','End apply@Simplex');
