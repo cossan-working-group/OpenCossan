d2c01f5603593394801eac67d6f0ac9d1783c318
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2020-03-16 19:40:54 +0100

    Initial rework of AdaptiveLineSampling
    Does not yet save the samples created during the line searches.

---
+++ adaptive.m
@@ -0,0 +1,32 @@
+x = opencossan.common.inputs.random.NormalRandomVariable('mean', 5, 'std', 2);
+y = opencossan.common.inputs.random.NormalRandomVariable('mean', 2, 'std', 2);
+
+demand = opencossan.common.inputs.Parameter('value', 0);
+
+input = opencossan.common.inputs.Input('members', {x y demand}, 'names', ["x" "y" "demand"]);
+
+mio = opencossan.workers.Mio('functionhandle', @boundary, 'isfunction', true, 'format', 'matrix', ...
+    'outputnames', {'g'}, 'inputnames', {'x' 'y'});
+
+evaluator = opencossan.workers.Evaluator('CXmembers', {mio}, 'CSmembers', {'mio'});
+
+model = opencossan.common.Model('Input', input, 'Evaluator', evaluator);
+
+performance = opencossan.reliability.PerformanceFunction('OutputName', 'Vg',...
+    'Demand', 'demand', 'Capacity', 'g');
+
+probModel = opencossan.reliability.ProbabilisticModel(...
+    'Model', model, 'PerformanceFunction', performance);
+
+mc = opencossan.simulations.MonteCarlo('samples', 10e6);
+pf_mc = mc.computeFailureProbability(probModel);
+
+als = opencossan.simulations.AdaptiveLineSampling('lines', 30);
+pf_als = als.computeFailureProbability(probModel);
+
+ls = opencossan.simulations.LineSampling('lines', 30, 'points', 1:7);
+pf_ls = ls.computeFailureProbability(probModel);
+
+function g = boundary(x)
+    g = -sqrt(x(:, 1).^2 + x(:, 2).^2) + 14;
+end
