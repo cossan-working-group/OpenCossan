fcadd48c861ab7de06868829a2cf5d25402f2d6d
Ander Gray <ander.gray@liverpool.ac.uk>
2021-12-15 22:45:28 +0000

    simplifies excell data

--- docs/tutorials/CredalNetworkFromData/get_data.m
+++ docs/tutorials/CredalNetworkFromData/get_data.m
@@ -14,22 +14,8 @@ excelsheet = "sample_excell.xlsx";
 
 [weather_states, weather_data] = read_data(excelsheet, "Weather Conditions");
 
-[hierarchy_states, hierarchy_data] = read_data(excelsheet, "Maintenance Hierarchy");
+[type_states, type_data] = read_data(excelsheet, "Road Type");
 
 [dist_type, dist_data] = read_data(excelsheet, "Disruption Type");
 
 [condition_type, condition_data] = read_data(excelsheet, "Road Condition");
-
-[distruption, distruption_data] = read_data(excelsheet, "Disruption Caused");
-
-% Sometimes further clean-up is required
-
-
-ids = weather_data  == "UNKNOWN";
-weather_data(ids) = "?";
-weather_states = setdiff(weather_states, "UNKNOWN");
-
-ids = distruption_data == "false";
-dist_data(ids) = "6 - NO DISRUPTION";
-
-dist_type = [dist_type; "6 - NO DISRUPTION"];
