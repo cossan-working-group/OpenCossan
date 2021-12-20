055716e7cdf840dd0ad278a2bb939a75d6e70af3
Jasper Behrensdorfm <jasper.behrensdorf@gmail.com>
2021-12-20 17:48:34 +0100

    Reduce tolerance for credal network integration tests

--- test/integration/+tutorials/TutorialCredalNetworkTest.m
+++ test/integration/+tutorials/TutorialCredalNetworkTest.m
@@ -5,7 +5,7 @@ classdef TutorialCredalNetworkTest < tutorials.TutorialTest
             "Marginal_BuiltIn.Report.UpperBound"];
         ExpectedValues = {[0.325423611686751; 0.641233722778278], ...
             [0.35877; 0.67458]};
-        Tolerance = [1e-5, 1e-5];
+        Tolerance = [1e-2, 1e-2];
         PreTest = "";
     end
 end
